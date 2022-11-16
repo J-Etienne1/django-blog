87caa9f320b8ccddde8184f62b205d7c6220f72e
Jason <jasonetienne1@gmail.com>
2022-11-16 20:40:34 +0000

    .

--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -3,14 +3,12 @@ from django.shortcuts import render
 
 
 def homepage(request):
-    # return HttpResponse('homepage')
     return render(request, 'homepage.html')
 
 
 def about(request):
-    # return HttpResponse('about')
     return render(request, 'about.html')
 
+
 def test(request):
-    # return HttpResponse('about')
     return render(request, 'test.html')
