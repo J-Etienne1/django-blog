fe87b44925cfe2c2a62a0cc8679db8b88d8c6b64
Jason <jasonetienne1@gmail.com>
2022-11-16 20:02:55 +0000

    added html templates

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -19,6 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', views.about ),
+    path('about/', views.about),
+    path('test/', views.test),
     path('', views.homepage),
 ]
--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 
 def homepage(request):
-    return HttpResponse('homepage')
+    # return HttpResponse('homepage')
+    return render(request, 'homepage.html')
+
 
 def about(request):
-    return HttpResponse('about')
+    # return HttpResponse('about')
+    return render(request, 'about.html')
+
+def test(request):
+    # return HttpResponse('about')
+    return render(request, 'test.html')
